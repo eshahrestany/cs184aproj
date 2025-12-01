d2ff924393c75c864246f03ad7ce68ae6d67162b
degibson12 <degibson@uci.edu>
2025-11-30 22:50:35 -0800

    all imports and downloading
    just downloading sklearn pretty much

--- cnn.py
+++ cnn.py
@@ -1,6 +1,22 @@
-import kagglehub
+#training a cnn model to determine melanoma type
 
-# Download latest version
+import kagglehub
+import os
+import argparse
+from pathlib import Path
+import time
+import copy
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms, models
+
+from sklearn.metrics import (
+    accuracy_score, precision_score, recall_score, f1_score,
+    roc_auc_score, confusion_matrix
+)
+
 path = kagglehub.dataset_download("hasnainjaved/melanoma-skin-cancer-dataset-of-10000-images")
 
-print("Path to dataset files:", path)
